bbb99b07ed3d6c62b7c76456618dde8c88d6c2a7
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-01-14 12:41:30 +0700

    First commit

---
+++ src/main/java/com/irdaislakhuafa/dev/springbootgdriveintegration/SpringBootGdriveIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.irdaislakhuafa.dev.springbootgdriveintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGdriveIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGdriveIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/irdaislakhuafa/dev/springbootgdriveintegration/SpringBootGdriveIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irdaislakhuafa.dev.springbootgdriveintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGdriveIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
